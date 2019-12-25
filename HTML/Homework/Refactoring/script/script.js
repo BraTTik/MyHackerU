628dd3dbf61abb8aa4f81cf06bc685cdd66158b8
BraTTik <bulki86@gmail.com>
2019-12-26 01:34:01 +0300

    cra build

--- HTML/Homework/Refactoring/script/script.js
+++
@@ -1,9 +0,0 @@
-let menuOpener = document.querySelector('.menu_menu-opener');
-let navElem = document.querySelector('.navigation_block');
-
-
-menuOpener.addEventListener('click', function() {
-    navElem.classList.toggle('active');
-    document.querySelector('.menu_menu-opener div').classList.toggle('active');
-    document.querySelector('.logo-block').classList.toggle('active');
-});
